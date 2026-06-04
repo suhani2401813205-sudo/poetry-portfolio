4b31de629f1f2ce30483f9fc26af42096f0a9008
suhani.2401813205-sudo <suhani.2401813205@vcet.edu.in>
2026-06-04 13:31:42 +0530

    connect to railway db

--- config/db.js
+++ config/db.js
@@ -5,12 +5,14 @@ let pool;
 async function getPool() {
   if (!pool) {
     pool = await mysql.createPool({
-      host: 'localhost',
-      user: 'root',
-      password: '9764',
-      database: 'poetry_portfolio',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       waitForConnections: true,
       connectionLimit: 10,
+      ssl: { rejectUnauthorized: false }
     });
   }
   return pool;
